e8b92dc97559e4bc41e9bff9080286c10d50f28d
Ezall2001 <armen.bakir.e@gmail.com>
2025-09-19 10:06:18 +0100

    ci: init telescope nvim plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,24 +1,31 @@
-local config = function()
-  local telescope = require('telescope')
-  local builtin = require('telescope.builtin')
-  local keymaps = require('keymap.definitions')
-  keymaps.telescope(builtin)
+local ts_config = {
+	defaults = {
+		sorting_strategy = "descending",
+		selection_strategy = "follow",
+		mappings = {},
+	},
 
-  telescope.setup {
-    extensions = {
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown {}
-      }
-    }
-  }
+	extensions = {
+
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+}
+
+local config = function()
+	local telescope = require("telescope")
+
+	telescope.setup(ts_config)
+
+	telescope.load_extension("fzf")
 end
 
 local spec = {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  config = config,
-  dependencies = { 'nvim-lua/plenary.nvim' }
-
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	config = config,
+	dependencies = { "nvim-lua/plenary.nvim" },
 }
 
 return spec
--- nvim/lua/plugins/telescope_fzf_native.lua
+++ nvim/lua/plugins/telescope_fzf_native.lua
@@ -1,6 +1,6 @@
 local spec = {
-  'nvim-telescope/telescope-fzf-native.nvim',
-  build = 'make'
+	"nvim-telescope/telescope-fzf-native.nvim",
+	build = "make",
 }
 
 return spec
