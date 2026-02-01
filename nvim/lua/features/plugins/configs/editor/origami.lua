53a3a07b376eca099325e3cc17e7d05a17e29788
Ezall2001 <armen.bakir.e@gmail.com>
2026-02-01 22:46:04 +0100

    feat(nvim): update origami

--- nvim/lua/features/plugins/configs/editor/origami.lua
+++ nvim/lua/features/plugins/configs/editor/origami.lua
@@ -1,26 +1,30 @@
-local i = require("constants.icons").text
-local o = require("origami")
+local i = require('constants.icons').text
+local o = require('origami')
 
 local config = {
-	useLspFoldsWithTreesitterFallback = true,
+	useLspFoldsWithTreesitterFallback = {
+		enabled = true,
+		foldmethodIfNeitherIsAvailable = 'indent', ---@type string|fun(bufnr: number): string
+	},
 	pauseFoldsOnSearch = true,
 	foldtext = {
 		enabled = true,
 		padding = 3,
 		lineCount = {
-			template = i.fold_lines(true) .. "%d",
-			hlgroup = "Comment",
+			template = i.fold_lines(true) .. '%d',
+			hlgroup = 'Comment',
 		},
 		diagnosticsCount = true,
 		gitsignsCount = true,
 	},
 	autoFold = {
 		enabled = true,
-		kinds = { "imports" }, ---@type lsp.FoldingRangeKind[]
+		kinds = { 'imports' }, ---@type lsp.FoldingRangeKind[]
 	},
 	foldKeymaps = {
 		setup = true,
-		hOnlyOpensOnFirstColumn = true,
+		closeOnlyOnFirstColumn = false,
+		scrollLeftOnCaret = false,
 	},
 }
 
