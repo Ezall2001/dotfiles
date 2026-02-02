9bdd631f298101eea20b479ef6c965536ae2a1a8
Ezall2001 <armen.bakir.e@gmail.com>
2026-02-02 17:58:47 +0100

    chore(nvim): format

--- nvim/lua/features/plugins/configs/editor/mini_surround.lua
+++ nvim/lua/features/plugins/configs/editor/mini_surround.lua
@@ -1,12 +1,12 @@
-local m = require("mini.surround")
+local m = require('mini.surround')
 
 local config = {
 	custom_surroundings = nil,
-	mappings = require("keymap.definitions.editor").mini_surround(),
+	mappings = require('keymap.definitions.editor').mini_surround(),
 	highlight_duration = 0,
 	n_lines = 100,
 	respect_selection_type = true,
-	search_method = "cover_or_next",
+	search_method = 'cover_or_next',
 	silent = true,
 }
 
