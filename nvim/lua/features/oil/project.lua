1b24106f3714fd0b7f00bb634e5a5097ffe73c08
Ezall2001 <armen.bakir.e@gmail.com>
2026-02-03 18:06:16 +0100

    fix(nvim): invoke oil on all buffers on startup

--- nvim/lua/features/oil/project.lua
+++ nvim/lua/features/oil/project.lua
@@ -1,26 +1,41 @@
-local invoke = require("features.oil.invoke")
-local p = require("features.plugins")
+local invoke = require('features.oil.invoke')
+local p = require('features.plugins')
 
 local M = {}
 
-local load = function()
-	local path = vim.api.nvim_buf_get_name(0)
-
-	if vim.fn.isdirectory(path) == 1 then
-		invoke()
+local handle_win = function(win)
+	local buf = vim.api.nvim_win_get_buf(win)
+	local path = vim.api.nvim_buf_get_name(buf)
+	if vim.fn.isdirectory(path) ~= 1 then
+		return
 	end
+
+	vim.api.nvim_set_current_win(win)
+	invoke()
+end
+
+local load = function()
+	local curr_win = vim.api.nvim_get_current_win()
+
+	for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+		for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+			handle_win(win)
+		end
+	end
+
+	vim.api.nvim_set_current_win(curr_win)
 end
 
 M.create = function() end
 
 M.load = function()
-	p.on_plugin_register("oil", load)
+	p.on_plugin_register('oil', load)
 end
 
 M.delete = function() end
 
 M.fallback = function()
-	p.on_plugin_register("oil", load)
+	p.on_plugin_register('oil', load)
 end
 
 return M
