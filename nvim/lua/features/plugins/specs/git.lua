ca9f11f6e81ded36a3b4f79f2e3243f9731e7cbe
Ezall2001 <armen.bakir.e@gmail.com>
2026-02-14 04:43:29 +0100

    feat(nvim): switch to dlyongemallo fork of diffview

--- nvim/lua/features/plugins/specs/git.lua
+++ nvim/lua/features/plugins/specs/git.lua
@@ -2,13 +2,13 @@
 
 local specs = {
 	{ 'lewis6991/gitsigns.nvim', name = 'gitsigns' },
-	{ 'sindrets/diffview.nvim', name = 'diffview' },
+	{ 'dlyongemallo/diffview.nvim', name = 'diffview' },
 	{
 		'NeogitOrg/neogit',
 		name = 'neogit',
 		dependencies = {
 			'nvim-telescope/telescope.nvim',
-			'sindrets/diffview.nvim',
+			'dlyongemallo/diffview.nvim',
 		},
 	},
 }
