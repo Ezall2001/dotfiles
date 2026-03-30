31205dbf380c36d05bb838dc8f7a95d293a8c16e
Ezall2001 <armen.bakir.e@gmail.com>
2026-03-30 12:48:49 +0100

    chore(qutebrowser): restructure bind files

--- qutebrowser/src/binds/__init__.py
+++ qutebrowser/src/binds/__init__.py
@@ -3,7 +3,7 @@ from qutebrowser.config.configfiles import ConfigAPI
 
 from lib.binds import Binds
 
-from .definitions import hint, modes, other, readline, tabs, window
+from .definitions import modes, other, readline, tabs
 
 
 def setup(config: ConfigAPI, c: ConfigContainer):
@@ -11,8 +11,6 @@ def setup(config: ConfigAPI, c: ConfigContainer):
 	c.bindings.key_mappings = {}
 
 	tabs.setup()
-	window.setup()
-	hint.setup()
 	other.setup()
 	readline.setup()
 	modes.setup()
