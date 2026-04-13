4e9458a8bb1449218a1a62cfb52f1854a5e3ab6e
Ezall2001 <armen.bakir.e@gmail.com>
2026-04-13 07:01:06 +0100

    feat(qutebrowser): configure fonts

--- qutebrowser/src/settings/global_settings/__init__.py
+++ qutebrowser/src/settings/global_settings/__init__.py
@@ -1,7 +1,8 @@
-from . import colors, content, general, hints, input, qt, tabs, ui
+from . import colors, content, fonts, general, hints, input, qt, tabs, ui
 
 
 def setup(c):
+	fonts.setup(c)
 	colors.setup(c)
 	general.setup(c)
 	qt.setup(c)
--- qutebrowser/src/settings/global_settings/fonts.py
+++ qutebrowser/src/settings/global_settings/fonts.py
@@ -1,114 +1,31 @@
-# Font used in the completion categories.
-# Type: Font
-# c.fonts.completion.category = 'bold default_size default_family'
+def setup(c):
+	c = c.fonts
 
-# Font used in the completion widget.
-# Type: Font
-# c.fonts.completion.entry = 'default_size default_family'
+	c.default_family = ['JetBrainsMono Nerd Font', 'Amiri', 'Fluent Emoji Color']
+	c.default_size = '13px'
 
-# Font used for the context menu. If set to null, the Qt default is
-# used.
-# Type: Font
-# c.fonts.contextmenu = None
-
-# Font used for the debugging console.
-# Type: Font
-# c.fonts.debug_console = 'default_size default_family'
-
-# Default font families to use. Whenever "default_family" is used in a
-# font setting, it's replaced with the fonts listed here. If set to an
-# empty value, a system-specific monospace default is used.
-# Type: List of Font, or Font
-# c.fonts.default_family = []
-
-# Default font size to use. Whenever "default_size" is used in a font
-# setting, it's replaced with the size listed here. Valid values are
-# either a float value with a "pt" suffix, or an integer value with a
-# "px" suffix.
-# Type: String
-# c.fonts.default_size = '10pt'
-
-# Font used for the downloadbar.
-# Type: Font
-# c.fonts.downloads = 'default_size default_family'
-
-# Font used for the hints.
-# Type: Font
-# c.fonts.hints = 'bold default_size default_family'
-
-# Font used in the keyhint widget.
-# Type: Font
-# c.fonts.keyhint = 'default_size default_family'
-
-# Font used for error messages.
-# Type: Font
-# c.fonts.messages.error = 'default_size default_family'
-
-# Font used for info messages.
-# Type: Font
-# c.fonts.messages.info = 'default_size default_family'
-
-# Font used for warning messages.
-# Type: Font
-# c.fonts.messages.warning = 'default_size default_family'
-
-# Font used for prompts.
-# Type: Font
-# c.fonts.prompts = 'default_size sans-serif'
-
-# Font used in the statusbar.
-# Type: Font
-# c.fonts.statusbar = 'default_size default_family'
-
-# Font used for selected tabs.
-# Type: Font
-# c.fonts.tabs.selected = 'default_size default_family'
-
-# Font used for unselected tabs.
-# Type: Font
-# c.fonts.tabs.unselected = 'default_size default_family'
-
-# Font used for tooltips. If set to null, the Qt default is used.
-# Type: Font
-# c.fonts.tooltip = None
-
-# Font family for cursive fonts.
-# Type: FontFamily
-# c.fonts.web.family.cursive = ''
-
-# Font family for fantasy fonts.
-# Type: FontFamily
-# c.fonts.web.family.fantasy = ''
-
-# Font family for fixed fonts.
-# Type: FontFamily
-# c.fonts.web.family.fixed = ''
-
-# Font family for sans-serif fonts.
-# Type: FontFamily
-# c.fonts.web.family.sans_serif = ''
-
-# Font family for serif fonts.
-# Type: FontFamily
-# c.fonts.web.family.serif = ''
-
-# Font family for standard fonts.
-# Type: FontFamily
-# c.fonts.web.family.standard = ''
-
-# Default font size (in pixels) for regular text.
-# Type: Int
-# c.fonts.web.size.default = 16
-
-# Default font size (in pixels) for fixed-pitch text.
-# Type: Int
-# c.fonts.web.size.default_fixed = 13
-
-# Hard minimum font size (in pixels).
-# Type: Int
-# c.fonts.web.size.minimum = 0
-
-# Minimum logical font size (in pixels) that is applied when zooming
-# out.
-# Type: Int
-# c.fonts.web.size.minimum_logical = 6
+	c.completion.category = 'bold default_size default_family'
+	c.completion.entry = 'default_size default_family'
+	c.contextmenu = 'default_size default_family'
+	c.debug_console = 'default_size default_family'
+	c.downloads = 'italic default_size default_family'
+	c.hints = 'bold default_size default_family'
+	c.keyhint = 'default_size default_family'
+	c.messages.error = 'default_size default_family'
+	c.messages.info = 'default_size default_family'
+	c.messages.warning = 'default_size default_family'
+	c.prompts = 'default_size sans-serif'
+	c.statusbar = 'default_size default_family'
+	c.tabs.selected = 'default_size default_family'
+	c.tabs.unselected = 'default_size default_family'
+	c.tooltip = 'default_size default_family'
+	c.web.family.cursive = 'default_family'
+	c.web.family.fantasy = 'default_family'
+	c.web.family.fixed = 'default_family'
+	c.web.family.sans_serif = 'default_family'
+	c.web.family.serif = 'default_family'
+	c.web.family.standard = 'default_family'
+	c.web.size.default = 16
+	c.web.size.default_fixed = 13
+	c.web.size.minimum = 0
+	c.web.size.minimum_logical = 6
