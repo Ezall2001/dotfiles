02c15bfb57f85a2427001273a3706d33b5adc07d
Ezall2001 <armen.bakir.e@gmail.com>
2026-05-07 10:59:44 +0100

    ci: add commitlint

---
+++ .commitlintrc.ts
@@ -0,0 +1,43 @@
+import type { UserConfig } from '@commitlint/types'
+import { RuleConfigSeverity } from '@commitlint/types'
+
+const RCS = RuleConfigSeverity
+
+let rules: UserConfig['rules'] = {
+	'body-leading-blank': [RCS.Error, 'always'],
+	'body-empty': [RCS.Warning, 'never'],
+	'body-max-line-length': [RCS.Error, 'always', 72],
+	'footer-leading-blank': [RCS.Warning, 'always'],
+	'footer-max-line-length': [RCS.Error, 'always', 72],
+	'header-max-length': [RCS.Error, 'always', 72],
+	'header-trim': [RCS.Error, 'always'],
+	'subject-empty': [RCS.Error, 'never'],
+	'scope-case': [
+		RCS.Error,
+		'always',
+		{
+			cases: ['lowercase', 'snake-case'],
+			delimiters: ['+'],
+		},
+	],
+	'scope-delimiter-style': [RCS.Error, 'always', ['+']],
+	'type-case': [RCS.Error, 'always', 'lower-case'],
+	'type-empty': [RCS.Error, 'never'],
+	'type-enum': [
+		RCS.Error,
+		'always',
+		['chore', 'ci', 'feat', 'fix', 'perf', 'refactor', 'style'],
+	],
+}
+
+let config: UserConfig = {
+	extends: [],
+	parserPreset: 'conventional-changelog-conventionalcommits',
+	formatter: '@commitlint/format',
+	rules: rules,
+	ignores: [],
+	defaultIgnores: true,
+	helpUrl: 'help yourself',
+}
+
+export default config
