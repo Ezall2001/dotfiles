c4a824e99a93ac885dd1390df79ba930fc27debd
Ezall2001 <armen.bakir.e@gmail.com>
2026-05-17 09:21:29 +0100

    chore(nvim): reset gg and G keymaps

--- nvim/lua/keymap/definitions/reset.lua
+++ nvim/lua/keymap/definitions/reset.lua
@@ -4,6 +4,8 @@ local reset = function()
 	m({ 'n', 'Q', '<nop>' })
 	m({ { 'i', 'n', 'v' }, '<C-p>', '<nop>' })
 	m({ { 'i', 'n', 'v' }, '<C-n>', '<nop>' })
+	m({ { 'n', 'x' }, 'gg', '<nop>' })
+	m({ { 'n', 'x' }, 'G', '<nop>' })
 end
 
 local M = {}
