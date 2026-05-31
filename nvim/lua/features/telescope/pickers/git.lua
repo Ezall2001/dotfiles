7d3d6fc441fc1f151a05fc23fbe4530041ae47c4
Ezall2001 <armen.bakir.e@gmail.com>
2026-05-31 06:21:57 +0100

    feat(nvim): configure git status telescope picker

--- nvim/lua/features/telescope/pickers/git.lua
+++ nvim/lua/features/telescope/pickers/git.lua
@@ -1,7 +1,22 @@
+local g = require('constants.icons').git
+
 local M = {}
 
 M.status = function()
-	require('telescope.builtin').git_status({})
+	require('telescope.builtin').git_status({
+		git_icons = {
+			added = g.file_staged(false),
+			changed = g.file_modified(false),
+			copied = g.file_untracked(false),
+			deleted = g.file_deleted(false),
+			renamed = g.file_renamed(false),
+			unmerged = g.file_modified(false),
+			untracked = g.file_untracked(false),
+		},
+		expand_dir = true,
+		use_file_path = false,
+		use_git_root = true,
+	})
 end
 
 M.bcommits = function()
