9ad448a3c094ac09a7ef392465620edc3f2feae0
Ezall2001 <armen.bakir.e@gmail.com>
2026-05-31 05:59:12 +0100

    fix(nvim): half scrolls correctly handles folds

--- nvim/lua/features/window/scroll.lua
+++ nvim/lua/features/window/scroll.lua
@@ -1,13 +1,11 @@
 local M = {}
 
 M.half_scroll = function(dir)
-	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
-
 	local win_height = vim.api.nvim_win_get_height(0)
 	local offset = math.floor((win_height - 1) / 2)
 
-	local new_cursor_pos = math.max(1, row + offset * dir)
-	vim.cmd('normal! ' .. new_cursor_pos .. 'Gzz')
+	local cmd = ('normal! %s%szz'):format(offset, dir)
+	vim.cmd(cmd)
 end
 
 return M
--- nvim/lua/keymap/definitions/editor/scrolling.lua
+++ nvim/lua/keymap/definitions/editor/scrolling.lua
@@ -6,13 +6,13 @@ local scrolling = function()
 	m({
 		{ 'n', 'v' },
 		'<C-d>',
-		u.mkcb(scroll.half_scroll, 1),
+		u.mkcb(scroll.half_scroll, 'j'),
 		{ desc = 'scroll down half page' },
 	})
 	m({
 		{ 'n', 'v' },
 		'<C-u>',
-		u.mkcb(scroll.half_scroll, -1),
+		u.mkcb(scroll.half_scroll, 'k'),
 		{ desc = 'scroll up half page' },
 	})
 	m({
